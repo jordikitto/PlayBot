463c2aaee5e24809f158ec65cd0b92a9b42847c9
Jordi Kitto RPI <jordikitto@gmail.com>
2019-05-03 18:52:01 +1000

    Updated motor driver, working

--- playbot_drive.py
+++ playbot_drive.py
@@ -1,92 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  1 03:03:12 2019
-PlayBot Drive/Launcher Functions
-@author: emile
-"""
-
-#importing lib
-import RPi.GPIO as GPIO
+import sys
 import time
+import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM) #set pin numbering to BCM system
-GPIO.setwarnings(False)
+mode=GPIO.getmode()
 
-#defining speeds in terms of duty cycles (can alter when testing)
-low_speed = 40
-med_speed = 70
-high_speed = 100
+GPIO.cleanup()
 
-#setting an initial speed
-speed = high_speed
+Forward_left=26
+Backward_left=21
+Forward_right = 16
+Backward_right = 20
 
-#initialise GPIO and  outputs
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Forward_left, GPIO.OUT)
+GPIO.setup(Backward_left, GPIO.OUT)
+GPIO.setup(Forward_right, GPIO.OUT)
+GPIO.setup(Backward_right, GPIO.OUT)
 
-#left motor
-GPIO.setup(26, GPIO.OUT)            #Assume initially this controls fwd motion
-GPIO.setup(16, GPIO.OUT)            #Assume initially this controls bwd motion
-pwm26 = GPIO.PWM(26, 100)			# 100Hz PWM Frequency is appropriate
-pwm16 = GPIO.PWM(16, 100)	
+def forward():
+    GPIO.output(Forward_left, GPIO.HIGH)
+    GPIO.output(Forward_right, GPIO.HIGH)  
 
-#right motor
-GPIO.setup(20, GPIO.OUT)            #Assume initially this controls fwd motion
-GPIO.setup(21, GPIO.OUT)            #Assume initially this controls bwd motion
-pwm20 = GPIO.PWM(20, 100)			# Initiate the PWM signal
-pwm21 = GPIO.PWM(21, 100)			# Initiate the PWM signal
+def backward():
+    GPIO.output(Backward_left, GPIO.HIGH)
+    GPIO.output(Backward_right, GPIO.HIGH)
 
-#launcher motor
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
+def turn_left():
+    GPIO.output(Backward_left, GPIO.HIGH)
+    GPIO.output(Forward_right, GPIO.HIGH)
 
-#LEDS
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
+def turn_right():
+    GPIO.output(Forward_left, GPIO.HIGH)
+    GPIO.output(Backward_right, GPIO.HIGH) 
 
-#Spares
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(19, GPIO.OUT)
-
-# Functions ------------
-    
-def forward(): #when forward button is pressed
-    #pwm26.start(speed)
-    #pwm20.start(speed)
-    GPIO.output(26, GPIO.HIGH)
-    GPIO.output(20, GPIO.HIGH)
-
-    
-def backward(): #when back button is pressed
-    #pwm16.start(speed)
-    #pwm21.start(speed)
-    GPIO.output(16, GPIO.HIGH)
-    GPIO.output(21, GPIO.HIGH)
-    
-def turn_right(): #for turning right while stationary
-    #pwm20.start(speed)
-    GPIO.output(20, GPIO.HIGH)
-
-def turn_left(): #for turning left while stationary
-    #pwm26.start(speed)
-    GPIO.output(26, GPIO.HIGH)
-    
-def turn_fwd_left(): #for both forward and left being pressed
-    pwm26.start(speed)
-    pwm20.start(speed-20)
-    
-def turn_fwd_right(): #for both forward and right being pressed
-    pwm26.start(speed-20)
-    pwm20.start(speed)
-    
-def stop(): #when no button is being pressed
-    #pwm26.stop()
-    #pwm20.stop()
-    #pwm16.stop()
-    #pwm21.stop()
-    GPIO.output(26, GPIO.LOW)
-    GPIO.output(20, GPIO.LOW)
-    GPIO.output(16, GPIO.LOW)
-    GPIO.output(21, GPIO.LOW)
-    
-    
+def stop():
+    GPIO.output(Forward_left, GPIO.LOW)
+    GPIO.output(Forward_right, GPIO.LOW) 
+    GPIO.output(Backward_left, GPIO.LOW)
+    GPIO.output(Backward_right, GPIO.LOW)
